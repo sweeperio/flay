402045a19883ab6a885c6077512920b2841aea8c
pseudomuto <david.muto@gmail.com>
2015-12-29 13:36:17 -0500

    adding coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
+require "simplecov"
+require "coveralls"
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter "shared/"
+end
+
 $LOAD_PATH.unshift File.expand_path("../lib", __FILE__)
 require "flay"
 require "pry"
